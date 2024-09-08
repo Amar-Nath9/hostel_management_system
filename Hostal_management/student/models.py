e3251db4707c9e97d400045cd4e51b0ce1296ba6
Amar-Nath9 <shivacodeamar@gmail.com>
2024-09-08 22:33:52 +0530

    created the payment_details module

--- Hostal_management/student/models.py
+++ Hostal_management/student/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Payment_details(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    amount_paid = models.DecimalField(max_digits=10,decimal_places=2)
+    payment_screenshot = models.ImageField(upload_to='payment_screenshots/')
+    payment_date = models.DateTimeField(auto_now_add=True)  # Automatically add the payment date
+
+    def __str__(self):
+        return f'{self.user.username} - {self.amount_paid}'
